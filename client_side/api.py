c61150efec423194732db1954560a08e35985fe7
jakob123100 <bokajnevs1@gmail.com>
2023-07-28 15:44:19 +0200

    something

--- client_side/api.py
+++ client_side/api.py
@@ -1,9 +1,5 @@
 import requests
-import datetime
-
-class data_packet:
-    date_time: datetime.datetime
-    value: float
+from datetime import datetime
 
 class data_fetcher:
     class datatype:
@@ -27,9 +23,13 @@ class data_fetcher:
 
         return response.json()
     
-    def __post_json_data(self, path, json_data: dict):
+    def __post_json_data(self, path, date_time: datetime, value: float):
+        date_time_string = str(date_time).replace(" ", "T")
+        date_time_string = date_time_string[:19]
+
         try:
-            print(self.__api_url + path)
+            #print(self.__api_url + path)
+            json_data = {"date_time": date_time_string, "value": value}
             response = requests.post(self.__api_url + path, json=json_data)
         except requests.exceptions.InvalidURL:
             print("Could not connect to api url")
@@ -55,20 +55,19 @@ class data_fetcher:
         temperature_data: dict = self.__request_json_data(f"{category}{self.__get_latest_api_path}")
         return temperature_data
     
-    def test_instet(self, datatype, data_packet: dict):
+    def test_instet(self, datatype, date_time: datetime, value: float):
         category = self.__get_category_string(datatype)
-        response = self.__post_json_data(f"{category}/InsertTest/", data_packet)
+        response = self.__post_json_data(f"{category}/InsertTest/", date_time, value)
         return response
 
     
 df = data_fetcher()
-dt = datetime.datetime.strptime("2023/07/27T16:30:22", "%Y/%m/%d %H:%M:%S")
-print(dt)
-dp = {"date_time": str(dt), "value": 24}
+dt = datetime.strptime("2023/07/27T16:30:22", "%Y/%m/%dT%H:%M:%S")
+dt = datetime.now()
 
 #dp.date_time = datetime.time
 #dp.value = 24
 
-print(df.test_instet(df.datatype.temperature, dp))
+print(df.test_instet(df.datatype.temperature, dt, 24))
 #last_temp_data = df.get_latest(df.datatype.temperature)
 #print(f"Time: {last_temp_data['Date']}, Temperature: {last_temp_data['Temperature']}")
